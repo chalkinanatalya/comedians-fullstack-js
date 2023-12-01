5585e4c0fb0b08ea30e79ac7968552af35d7d9f0
Natalya <m_d.r.lecter@protonmail.com>
2023-12-01 22:38:16 +0400

    add client

--- api-standup/index.js
+++ api-standup/index.js
@@ -3,10 +3,11 @@ import fs from "node:fs/promises";
 import { sendData, sendError } from "./modules/send.js";
 import { checkFile } from "./modules/checkFile.js";
 import { handleComediansRequest } from "./modules/handleComediansRequest.js";
+import { handleAddClient } from "./modules/handleAddClient.js";
 
 const PORT = 8080;
 const COMEDIANS = './comedians.json';
-const CLIENTS = './clients.json';
+export const CLIENTS = './clients.json';
 
 const startServer = async () => {
     if(!(await checkFile(COMEDIANS))) {
@@ -30,22 +31,29 @@ const startServer = async () => {
             } 
             
             if(req.method === 'POST' && segments[0] == 'clients') {
+                handleAddClient(req, res);
+                return;
                 // POST /clients
             }
     
             if(req.method === 'GET' && segments[0] == 'clients' && segments.length === 2) {
+                const ticket = segments[1];
+                handleAddClient(req, res, ticket);
+                return;
                 // GET /clients/:ticket
                 //get client by ticket number
             }
     
             if(req.method === 'PATCH' && segments[0] == 'clients' && segments.length === 2) {
+                handleUpdateClient(req, res, segments);
+                return;
                 // PATCH /clients/:ticket
                 //edit client by ticket number
             }
 
             sendError(res, 404, '404 Page is not found');
         } catch (error) {
-            sendError(res, 500, `Server error: ${err.message}`);
+            sendError(res, 500, `Server error: ${error.message}`);
         }
     }).listen(PORT);
 
@@ -53,3 +61,5 @@ const startServer = async () => {
 }
 
 startServer();
+
+
